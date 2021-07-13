95636f41217d496744c8bfec91cdbe909b5a9972
SirSilverfrost <86638531+SirSilverfrost@users.noreply.github.com>
2021-07-13 01:41:34 -0400

    Added Pillar Treasure Bags

---
+++ Items/Bags/VortexTreasureBag.cs
@@ -0,0 +1,36 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RedboidMutilation.Items.Bags
+{
+    public class VortexBag : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Treasure Bag");
+            Tooltip.SetDefault("Right click to open");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 32;
+            item.rare = -12;
+            item.maxStack = 999;
+            item.value = 1;
+            item.expert = true;
+            item.expertOnly = false;
+        }
+
+        public override bool CanRightClick()
+        {
+            return true;
+        }
+
+        public override void RightClick(Player player)
+        {
+            player.QuickSpawnItem(ItemID.FragmentVortex, 24 + Main.rand.Next(76)); // 5, 6, or 7);
+        }
+    }
+}
---
+++ NPCs/NPCs.cs
@@ -0,0 +1,68 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RedboidMutilation.NPCs
+{
+	class MyGlobalNPC : GlobalNPC
+	{
+		public override void NPCLoot(NPC npc)
+		{
+			if (npc.type == NPCID.UndeadMiner)
+			{
+				if (Main.rand.NextFloat() < .1200f)
+					Item.NewItem(npc.getRect(), mod.ItemType("RustedHamaxe"));
+			}
+			if (npc.type == NPCID.LunarTowerSolar)
+			{
+				if (Main.expertMode)
+				npc.DropItemInstanced(npc.position, npc.Size, mod.ItemType("SolarBag"));
+			}
+			if (npc.type == NPCID.LunarTowerNebula)
+			{
+				if (Main.expertMode)
+					npc.DropItemInstanced(npc.position, npc.Size, mod.ItemType("NebulaBag"));
+			}
+			if (npc.type == NPCID.LunarTowerVortex)
+			{
+				if (Main.expertMode)
+					npc.DropItemInstanced(npc.position, npc.Size, mod.ItemType("VortexBag"));
+			}
+			if (npc.type == NPCID.LunarTowerStardust)
+			{
+				if (Main.expertMode)
+					npc.DropItemInstanced(npc.position, npc.Size, mod.ItemType("StardustBag"));
+			}
+		}
+		public override bool PreNPCLoot(NPC npc)
+		{
+			if (npc.type == NPCID.LunarTowerSolar)
+			{
+				if (Main.expertMode)
+				NPCLoader.blockLoot.Add(ItemID.FragmentSolar);
+			}
+			
+		
+		    if (npc.type == NPCID.LunarTowerNebula)
+		    {
+				if (Main.expertMode)
+				NPCLoader.blockLoot.Add(ItemID.FragmentNebula);
+	        }
+			
+
+			if (npc.type == NPCID.LunarTowerVortex)
+			{
+				if (Main.expertMode)
+					NPCLoader.blockLoot.Add(ItemID.FragmentVortex);
+			}
+			
+
+			if (npc.type == NPCID.LunarTowerStardust)
+			{
+				if (Main.expertMode)
+					NPCLoader.blockLoot.Add(ItemID.FragmentStardust);
+			}
+			return true;
+		}
+	}
+}
