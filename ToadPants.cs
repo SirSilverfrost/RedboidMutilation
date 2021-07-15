0e2cac85109b80caf2c5ad1be43820c731be4be2
SirSilverfrost <86638531+SirSilverfrost@users.noreply.github.com>
2021-07-15 04:50:34 -0400

    Add files via upload

---
+++ ToadPants.cs
@@ -0,0 +1,28 @@
+﻿using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace RedboidMutilation.Items.Vanity
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class MushroomPants : ModItem
+	{
+		public override void SetDefaults()
+		{
+			item.width = 30;
+			item.height = 30;
+			item.rare = 1;
+			item.vanity = true;
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.Mushroom, 6);
+			recipe.AddIngredient(ItemID.GlowingMushroom, 20);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+    }
+}
