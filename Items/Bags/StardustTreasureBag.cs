5cab211f0ac953a060d31c8aa15d34559a62e976
SirSilverfrost <86638531+SirSilverfrost@users.noreply.github.com>
2021-07-15 17:37:13 -0400

    Update StardustTreasureBag.cs

--- Items/Bags/StardustTreasureBag.cs
+++ Items/Bags/StardustTreasureBag.cs
@@ -35,13 +35,8 @@ namespace RedboidMutilation.Items.Bags
             player.TryGettingDevArmor();
             if (Calamity == null)
             player.QuickSpawnItem(ItemID.FragmentStardust, 30 + Main.rand.Next(60)); // 5, 6, or 7);
-<<<<<<< Updated upstream
             if (Main.rand.Next(7) < 1) // a 1 in 7 chance
             player.QuickSpawnItem(ModContent.ItemType<StargazerMask>());
-        } 
-    }
-}
-=======
             if (Calamity != null)
             player.QuickSpawnItem(ItemID.FragmentStardust, 5 + Main.rand.Next(10));
             if (Main.rand.Next(7) < 1) // a 1 in 7 chance
@@ -49,5 +44,3 @@ namespace RedboidMutilation.Items.Bags
         }
     }
 }
-
->>>>>>> Stashed changes
