cf824c4c323d0506acb0d9336102da46fe06feef
SirSilverfrost <86638531+SirSilverfrost@users.noreply.github.com>
2021-07-15 17:40:41 -0400

    Update ToadPants.cs

--- Items/Vanity/ToadPants.cs
+++ Items/Vanity/ToadPants.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 ﻿using Terraria.ID;
 using Terraria;
 using Terraria.ModLoader;
@@ -24,35 +23,6 @@ namespace RedboidMutilation.Items.Vanity
 			recipe.AddTile(TileID.Anvils);
 			recipe.SetResult(this);
 			recipe.AddRecipe();
-		}
-    }
-=======
-﻿using Terraria.ID;
-using Terraria;
-using Terraria.ModLoader;
-
-namespace RedboidMutilation.Items.Vanity
-{
-	[AutoloadEquip(EquipType.Legs)]
-	public class MushroomPants : ModItem
-	{
-		public override void SetDefaults()
-		{
-			item.width = 30;
-			item.height = 30;
-			item.rare = 1;
-			item.vanity = true;
-		}
-
-		public override void AddRecipes()
-		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.Mushroom, 6);
-			recipe.AddIngredient(ItemID.GlowingMushroom, 20);
-			recipe.AddTile(TileID.Loom);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
-		}
-    }
->>>>>>> Stashed changes
+                 }
+         }
 }
