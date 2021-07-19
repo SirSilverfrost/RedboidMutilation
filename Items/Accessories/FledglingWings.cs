2fb50203352db6be5740b877c1852580e4c504f8
SirSilverfrost <86638531+SirSilverfrost@users.noreply.github.com>
2021-07-18 23:43:55 -0400

    Sky High Update

---
+++ Items/Accessories/FledglingWings.cs
@@ -0,0 +1,45 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace RedboidMutilation.Items.Accessories
+{
+	[AutoloadEquip(EquipType.Wings)]
+	public class FledglingWings : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("This is a modded wing.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 22;
+			item.height = 20;
+			item.value = 10000;
+			item.rare = ItemRarityID.Green;
+			item.accessory = true;
+		}
+		//these wings use the same values as the solar wings
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.wingTimeMax = 25;
+		}
+
+		public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+			ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+		{
+			ascentWhenFalling = 0.85f;
+			ascentWhenRising = 0.15f;
+			maxCanAscendMultiplier = 1f;
+			maxAscentMultiplier = 1f;
+			constantAscend = 0.135f;
+		}
+
+		public override void HorizontalWingSpeeds(Player player, ref float speed, ref float acceleration)
+		{
+			speed *= 1f;
+			acceleration *= 1f;
+		}
+	}
+}
---
+++ Items/Misc/ScarabFishingPole.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using RedboidMutilation.Items.Misc;
+
+namespace RedboidMutilation.Items.Misc
+{
+	public class ScarabFishingPole : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Scarab Fishing Pole");
+			Tooltip.SetDefault("'Made of hard chitin, it looks sturdy enough to endure lava'");
+			ItemID.Sets.CanFishInLava[item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			item.useStyle = 1;
+			item.useAnimation = 8;
+			item.useTime = 8;
+			item.width = 48;
+			item.height = 48;
+			item.UseSound = SoundID.Item1;
+
+			//Sets the poles fishing power
+			item.fishingPole = 38;
+
+			//Sets the speed in which the bobbers are launched, Wooden Fishing Pole is 9f and Golden Fishing Rod is 17f
+			item.shootSpeed = 13f;
+
+			//The Bobber projectile
+			item.shoot = ModContent.ProjectileType<Projectiles.ScarabBobber>();
+		}
+
+	}
+}
